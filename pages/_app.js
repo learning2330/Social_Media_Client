fd1ae7bbadd4175b00ae52c88489a4049d4aca38
learning2330 <learning.test2330@gmail.com>
2023-07-31 11:39:23 -0700

    Design register page

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "../compoonents/Navbar";
+import Head from "next/head";
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="stylesheet" href="/css/style.css"></link>
+      </Head>
       <Navbar />
       <Component {...pageProps} />
     </>
