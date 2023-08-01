415fb520a0b14f40bfbc14a6499f16112d55923b
learning2330 <learning.test2330@gmail.com>
2023-08-01 10:45:18 -0700

    Some more code

--- pages/register.js
+++ pages/register.js
@@ -1,6 +1,25 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 export default function register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [secret, setSecret] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(name, email, password, secret);
+    axios
+      .post("http://localhost:8000/api/register", {
+        name,
+        email,
+        password,
+        secret,
+      })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="container-flux">
       <div className="row py-5 bg-secondary text-light">
@@ -10,12 +29,14 @@ export default function register() {
       </div>
       <div className="row">
         <div className="col-md-6 offset-md-3">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="form-group p-2">
               <small>
                 <label className="text-muted">Your Name</label>
               </small>
               <input
+                value={name}
+                onChange={(e) => setName(e.target.value)}
                 text="text"
                 className="form-control"
                 placeholder="Enter Name"
@@ -26,6 +47,8 @@ export default function register() {
                 <label className="text-muted">Email</label>
               </small>
               <input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 text="email"
                 className="form-control"
                 placeholder="Enter Email"
@@ -36,6 +59,8 @@ export default function register() {
                 <label className="text-muted">Password</label>
               </small>
               <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 text="password"
                 className="form-control"
                 placeholder="Enter Password"
@@ -56,6 +81,8 @@ export default function register() {
             </div>
             <div className="form-group p-2">
               <input
+                value={secret}
+                onChange={(e) => setSecret(e.target.value)}
                 type="text"
                 className="form-control"
                 placeholder="Write yourr answer"
